42a70a0162479da4da17152e82020d145b769434
Autunnoo <68438045+Autunnoo@users.noreply.github.com>
2020-09-15 15:49:43 +0300

    Updates

---
+++ precache-manifest.50114b23c317f51990363ca2c06726fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcb0b898e341a8735166a8b2988fdc8",
+    "url": "/hw15/index.html"
+  },
+  {
+    "revision": "d449f51877db06316053",
+    "url": "/hw15/static/css/main.7ed2b125.chunk.css"
+  },
+  {
+    "revision": "e9f33d67391be3dd6caf",
+    "url": "/hw15/static/js/2.1de5aa8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw15/static/js/2.1de5aa8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d449f51877db06316053",
+    "url": "/hw15/static/js/main.b1112ce2.chunk.js"
+  },
+  {
+    "revision": "5b5ab7aafb734b95630c",
+    "url": "/hw15/static/js/runtime-main.ff0b5264.js"
+  }
+]);
